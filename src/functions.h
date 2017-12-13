5d80f26f9fe5218c062beb948504501693900517
tkgit123 <ako_tako@hotmail.com>
2017-12-13 17:21:18 +0100

    Added functions.h to src folder.

---
+++ src/functions.h
@@ -0,0 +1,14 @@
+
+int chk_input(long float);
+
+
+long float add(long float, long float);
+
+
+long float sub(long float, long float);
+
+
+long float mult(long float, long float);
+
+
+long float div(long float, long float);
